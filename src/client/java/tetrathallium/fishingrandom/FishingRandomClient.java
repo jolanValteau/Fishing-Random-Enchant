8b6e8c2067ce49f737f945716867318b9c9238d9
jolan <jolan.valteau@gmail.com>
2024-12-12 16:47:35 +0100

    Code (#1)

---
+++ src/client/java/tetrathallium/fishingrandom/FishingRandomClient.java
@@ -0,0 +1,11 @@
+package tetrathallium.fishingrandom;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FishingRandomClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		FishingEventHandler.registerFishingEvent();
+	}
+}
