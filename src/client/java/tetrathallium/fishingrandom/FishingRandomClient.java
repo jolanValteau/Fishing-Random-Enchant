8c1adb7bde225295f0f504bbf38679a5dd37c3da
jolanValteau <jolan.valteau@gmail.com>
2024-12-13 02:03:46 +0100

    Moved from Client to Main execution

--- src/client/java/tetrathallium/fishingrandom/FishingRandomClient.java
+++ src/client/java/tetrathallium/fishingrandom/FishingRandomClient.java
@@ -1,19 +1,9 @@
 package tetrathallium.fishingrandom;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.enchantment.Enchantment;
-
-import net.minecraft.util.Identifier;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.Registries;
 
 
 public class FishingRandomClient implements ClientModInitializer {
-
-	public static final Enchantment GAMBLING = new GamblingEnchantment();
-
 	@Override
-	public void onInitializeClient() {
-		Registry.register(Registries.ENCHANTMENT, new Identifier("fishingrandom", "gambling"), GAMBLING);
-	}
+	public void onInitializeClient() {}
 }
