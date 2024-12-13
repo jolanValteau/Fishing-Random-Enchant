5e9a878f46bba54aab0a7e3c84015aaa46325ed9
jolan <jolan.valteau@gmail.com>
2024-12-13 02:05:28 +0100

    Upgrade 0.1.0 alpha (#2)
    * Clean up + runClient data + Enchantment class base
    
    * Moved from Client to Main execution

--- src/main/java/tetrathallium/fishingrandom/mixin/FishingBobberEntityMixin.java
+++ src/main/java/tetrathallium/fishingrandom/mixin/FishingBobberEntityMixin.java
@@ -1,44 +1,35 @@
 package tetrathallium.fishingrandom.mixin;
 
-import net.minecraft.text.Text;
 import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.FishingRodItem;
 import net.minecraft.item.Item;
 
 import java.util.List;
+
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
 import net.minecraft.registry.Registries;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(FishingBobberEntity.class)
+@Mixin(FishingRodItem.class)
 public class FishingBobberEntityMixin {
     
-    @Inject(method = "onFishingSuccess", at = @At("HEAD"))
-    private void onFishingSuccess(ItemStack caughtStack, CallbackInfo ci) {
-
-        FishingBobberEntity bobber = (FishingBobberEntity) (Object) this;
-        Entity owerEntity = bobber.getOwner();
-
-        if (!(owerEntity instanceof PlayerEntity)) {
-            return;
-        }
-        PlayerEntity player = (PlayerEntity) owerEntity;
-
-        if (player != null) {
-            player.sendMessage(Text.literal("Tu as pêché : " + caughtStack.getName().getString()), false);
-
-            randomFishingFunction(player, caughtStack);
-        }
+    @Inject(method = "use", at = @At("HEAD"))
+    private void onUse(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> ci) {
+        ItemStack rod = user.getStackInHand(hand);
+        randomFishingFunction(user, rod);
     }
 
-    private static void randomFishingFunction(PlayerEntity player, ItemStack caughtStack) {
-        player.sendMessage(Text.of("Custom called"), false);
+    private static void randomFishingFunction(PlayerEntity player, ItemStack rod) {
         giveRandomItem(player);
     }
 
