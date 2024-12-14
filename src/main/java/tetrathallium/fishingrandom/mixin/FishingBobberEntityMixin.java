ef692d1304b38a63689da4956b7053f2397d4dd4
jolan <jolan.valteau@gmail.com>
2024-12-14 01:24:40 +0100

    Upgrade 0.1.1 alpha (#3)
    * Update 0.1.1-alpha
    
    * Delete run directory
    
    * Added Fishing Caught Fish missing condition
    
    * More Memory + Some debug TODO REMOVE + gambling working
    TODO: Upgrade auto du level
    
    * Convert to tabs and LF + gpg

--- src/main/java/tetrathallium/fishingrandom/mixin/FishingBobberEntityMixin.java
+++ src/main/java/tetrathallium/fishingrandom/mixin/FishingBobberEntityMixin.java
@@ -2,7 +2,6 @@ package tetrathallium.fishingrandom.mixin;
 
 import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.FishingRodItem;
 import net.minecraft.item.Item;
@@ -13,7 +12,10 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+import tetrathallium.fishingrandom.GamblingEnchantment;
 import net.minecraft.registry.Registries;
+// import net.minecraft.entity.Entity;
+// import net.minecraft.text.Text;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,28 +24,42 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(FishingRodItem.class)
 public class FishingBobberEntityMixin {
-    
-    @Inject(method = "use", at = @At("HEAD"))
-    private void onUse(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> ci) {
-        ItemStack rod = user.getStackInHand(hand);
-        randomFishingFunction(user, rod);
-    }
+	
+	@Inject(method = "use", at = @At("HEAD"))
+	private void onUse(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> ci) {
+		ItemStack rod = user.getStackInHand(hand);
+		handleFishingGambling(world, user, rod);
+	}
 
-    private static void randomFishingFunction(PlayerEntity player, ItemStack rod) {
-        giveRandomItem(player);
-    }
+	private static void handleFishingGambling(World world, PlayerEntity player, ItemStack rod) {
+		boolean isGamblingTime = false;
+		for (FishingBobberEntity fishingBobber : world.getEntitiesByClass(FishingBobberEntity.class, player.getBoundingBox().expand(32), bobber -> true)) {
+			boolean caughtFishBool = ((FishingBobberEntityAccessor) fishingBobber).getCaughtFish();
+			if (!caughtFishBool) {continue;}
+			isGamblingTime = true;
+			break;
+		}
+		if (!isGamblingTime) {return;}
 
-    private static void giveRandomItem(PlayerEntity player)
-    {
-        List<Item> allItems = Registries.ITEM.stream().toList();
-        if (allItems.isEmpty())
-        {return;}
+		// Gambling time !
+		if (!GamblingEnchantment.SuccessGambling(rod, player)) {return;}
 
-        Random random = Random.create();
-        Item randomItem = allItems.get(random.nextInt(allItems.size()));
+		// Success
+		// player.sendMessage(Text.of("You got a random item !"), true); // Todo uncomment
+		giveRandomItem(player);
+	}
 
-        ItemStack randomStack = new ItemStack(randomItem);
+	private static void giveRandomItem(PlayerEntity player)
+	{
+		List<Item> allItems = Registries.ITEM.stream().toList();
+		if (allItems.isEmpty())
+		{return;}
 
-        player.giveItemStack(randomStack);
-    }
+		Random random = Random.create();
+		Item randomItem = allItems.get(random.nextInt(allItems.size()));
+
+		ItemStack randomStack = new ItemStack(randomItem);
+
+		player.giveItemStack(randomStack);
+	}
 }
